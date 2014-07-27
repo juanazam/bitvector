a1126d5f764d546eb7c624dbdfceeb449b6f537a
Adrian Mugnolo <adrian@wyeworks.com>
2014-07-26 22:32:14 -0300

    Improve documentation

--- lib/bit_vector.rb
+++ lib/bit_vector.rb
@@ -4,39 +4,48 @@ module BitVector
   class BitVector
     VECTOR_LAST_INDEX = 31
 
-    def initialize(num = 0)
+    # Returns new bit vector initialized to optional number.
+    def initialize(number = 0)
       @array = []
       VECTOR_LAST_INDEX.downto(0) do |i|
-        @array << num[i]
+        @array << number[i]
       end
     end
 
+    # Returns a string representation.
     def to_s
       @array.join('')
     end
 
+    # Returns an integer representation.
     def to_i
       to_s.to_i(2)
     end
 
+    # Sets the element at index.
     def []=(index, value)
       array[VECTOR_LAST_INDEX - index] = value
     end
 
+    # Returns the element at index.
     def [](index)
       array[VECTOR_LAST_INDEX - index]
     end
 
-    def self.load(val)
-      new(val.to_i)
+    # Loads vector from value.
+    def self.load(value)
+      new(value.to_i)
     end
 
-    def self.dump(bit_vector)
-      bit_vector.to_i
+    # Dumps vector to value.
+    def self.dump(vector)
+      vector.to_i
     end
 
-    def ==(other_vector)
-      array == other_vector.array
+    # Returns true if equal to other vector. Two vectors are considered equal if
+    # their arrays are equal.
+    def ==(other)
+      array == other.array
     end
 
     protected
